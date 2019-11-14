f0dc045b5a7dd55ace6c8199b8bfd79f7c0d4a0c
Jonathan <yoniyaniv@gmail.com>
2019-11-14 12:59:29 -0500

    added messages, only send refreshes

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,21 @@
+class MessagesController < ApplicationController
+  def new
+    @message = Message.new()
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_url
+    else
+      render json: @messages.erorrs.full_messages
+    end
+  end
+
+  private
+
+    def message_params
+      params.require(:message).permit(:message)
+    end
+
+end
